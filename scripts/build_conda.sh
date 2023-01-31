c5952a75b9f60cb9de32df8296c0203ed759df41
thraneh <thraneh@gmail.com>
2023-01-31 06:37:50 +0100

    fix build script

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ ! -z $CONDA_PREFIX ]]; then
-  (>&2 echo -e "\033[1;31mERROR: Please deactivate your conda environment before using this script. Use 'conda deactivate'.\033[0m") && exit 1
+if [[ -z $CONDA_PREFIX ]]; then
+  (>&2 echo -e "\033[1;31mERROR: Please activate your conda environment before using this script.\033[0m") && exit 1
 fi
 
 TARGET="$1"
@@ -20,7 +20,7 @@ esac
 
 echo -e "\033[1;34mBuild...\033[0m"
 
-conda build --channel "https://roq-trading.com/conda/$TARGET"
+conda build --channel "https://roq-trading.com/conda/$TARGET" conda/
 
 echo -e "\033[1;34mDone!\033[0m"
 echo -e "\033[1;34mYou can find the package in $CONDA_PREFIX/conda-bld'.\033[0m"
