9df791ac923b6cd5d9a8a8fd2c985360d36b3f85
thraneh <thraneh@gmail.com>
2024-11-19 16:14:24 +0100

    New simulator requiring interfaces from the roq-algo framework (#478)

--- src/simple/application.cpp
+++ src/simple/application.cpp
@@ -14,15 +14,6 @@ using namespace std::chrono_literals;  // NOLINT
 
 namespace simple {
 
-// === CONSTANTS ===
-
-namespace {
-auto const SNAPSHOT_FREQUENCY = 1s;
-auto const MATCHER = "simple"sv;  // note! filled when market is crossed
-auto const MARKET_DATA_LATENCY = 1ms;
-auto const ORDER_MANAGEMENT_LATENCY = 10ms;
-}  // namespace
-
 // === IMPLEMENTATION ===
 
 int Application::main(roq::args::Parser const &args) {
@@ -35,29 +26,8 @@ int Application::main(roq::args::Parser const &args) {
   }
   Settings settings{args};
   Config config{settings};
-  if (settings.simulation) {
-    simulation(settings, config, params);
-  } else {
-    trading(settings, config, params);
-  }
-  return EXIT_SUCCESS;
-}
-
-void Application::simulation(Settings const &settings, Config const &config, std::span<std::string_view const> const &params) {
-  auto collector = roq::client::detail::SimulationFactory::create_collector(SNAPSHOT_FREQUENCY);
-  auto create_generator = [&params](auto source_id) { return roq::client::detail::SimulationFactory::create_generator(params[source_id], source_id); };
-  auto create_matcher = [](auto &dispatcher) { return roq::client::detail::SimulationFactory::create_matcher(dispatcher, MATCHER); };
-  auto factory = roq::client::Simulator::Factory{
-      .create_generator = create_generator,
-      .create_matcher = create_matcher,
-      .market_data_latency = MARKET_DATA_LATENCY,
-      .order_management_latency = ORDER_MANAGEMENT_LATENCY,
-  };
-  roq::client::Simulator{settings, config, factory, *collector}.dispatch<value_type>(settings);
-}
-
-void Application::trading(Settings const &settings, Config const &config, std::span<std::string_view const> const &params) {
   roq::client::Trader{settings, config, params}.dispatch<value_type>(settings);
+  return EXIT_SUCCESS;
 }
 
 }  // namespace simple
